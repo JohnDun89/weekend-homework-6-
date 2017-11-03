263ebf4ba3d4952b497b1811ae8b3f1716499b6e
John Duncan <john@johnduncanphoto.com>
2017-11-03 15:15:13 +0000

    bass guitar can return noise test passed

---
+++ app/src/test/java/com/example/johnd/musicexchange/BassGuitarTest.java
@@ -0,0 +1,25 @@
+package com.example.johnd.musicexchange;
+
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static junit.framework.Assert.assertEquals;
+/**
+ * Created by JohnD on 03/11/2017.
+ */
+
+public class BassGuitarTest {
+
+    BassGuitar bassguitar;
+
+    @Before
+    public void before(){
+        bassguitar = new BassGuitar();
+    }
+
+    @Test
+    public void canBassGuitarReturnNoise(){
+        assertEquals("Ding Ding", bassguitar.instrumentNoise());
+    }
+}
